2b35854f52a22a3fd136d17726764fad2ffc6856
Divyengamit <divyengamit@inventam.com>
2022-06-17 14:45:07 +0530

    Axios config

--- services/axiosConfig.js
+++ services/axiosConfig.js
@@ -1,106 +1,129 @@
-/* eslint-disable no-undef */
-// import { toast } from "react-toastify";
 import axios from "axios";
-// import { API_BASE_URL } from "../config";
-
-// import "react-toastify/dist/ReactToastify.min.css";
-// import { ErrorToast } from "../components/Toast/Toast";
-const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
-export const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
+import { getLocal } from "../utils/storage";
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
 });
-
-// For Refreshing Token
-let isAlreadyFetchingAccessToken = false;
-
-// For Refreshing Token
-let subscribers = [];
-
-// Add a request interceptor
 axiosInstance.interceptors.request.use(function (config) {
-  const token = localStorage.getItem("access_token");
-  // localStorage.getItem("set_old_access_token")
+  console.log("config", config);
+  const token = getLocal("access_token");
   config.headers = {
     "Content-Type": "application/json",
   };
-  if (token) config.headers.Authorization = `${token}`;
+  // if (token) {
+  //   config.headers["Authorization"] = `Bearer ${token}`;
+  // }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  console.log("token get", config);
   return config;
 });
 
-// Add a response interceptor || Middleware for 401
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const {
-      config,
-      response: { status },
-    } = error;
-    const originalRequest = config;
-    if (status === 401) {
-      const refresh_token = localStorage.getItem("refresh_token");
-      if (refresh_token) {
-        // Refresh Token Promise
-        if (!isAlreadyFetchingAccessToken) {
-          isAlreadyFetchingAccessToken = true;
+export default axiosInstance;
 
-          refreshAccessToken(refresh_token)
-            .then((response) => {
-              isAlreadyFetchingAccessToken = false;
-              // Replacing Tokens
-              localStorage.removeItem("access_token");
-              localStorage.removeItem("refresh_token");
-              localStorage.setItem(
-                "access_token",
-                response.data.data.access_token
-              );
-              localStorage.setItem(
-                "refresh_token",
-                response.data.data.refresh_token
-              );
-              onAccessTokenFetched(response.data.data.access_token);
-            })
-            .catch(() => {
-              isAlreadyFetchingAccessToken = false;
-              // Removing User State
-              localStorage.removeItem("access_token");
-              localStorage.removeItem("refresh_token");
+// /* eslint-disable no-undef */
+// // import { toast } from "react-toastify";
+// import axios from "axios";
+// // import { API_BASE_URL } from "../config";
 
-              // showToast("Error !", error.response.data.message, "error");
-              // toast.error(ErrorToast(error.response.data.message), {
-              //   hideProgressBar: true,
-              //   autoClose: "100",
-              // });
-              setTimeout(() => {
-                window.location.reload();
-              }, 500);
-            });
-        }
+// // import "react-toastify/dist/ReactToastify.min.css";
+// // import { ErrorToast } from "../components/Toast/Toast";
+// const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+// export const axiosInstance = axios.create({
+//   baseURL: API_BASE_URL,
+// });
 
-        const retryOriginalRequest = new Promise((resolve) => {
-          addSubscriber((accessToken) => {
-            // Make sure to assign accessToken according to your response.
-            // Change Authorization header
-            originalRequest.headers["Authorization"] = `${accessToken}`;
-            resolve(axios(originalRequest));
-          });
-        });
-        return retryOriginalRequest;
-      } else {
-        return Promise.reject(error);
-      }
-    } else {
-      return Promise.reject(error);
-    }
-  }
-);
+// // For Refreshing Token
+// let isAlreadyFetchingAccessToken = false;
 
-const onAccessTokenFetched = (accessToken) => {
-  subscribers = subscribers.filter((callback) => callback(accessToken));
-};
+// // For Refreshing Token
+// let subscribers = [];
 
-const addSubscriber = (callback) => {
-  subscribers.push(callback);
-};
+// // Add a request interceptor
+// axiosInstance.interceptors.request.use(function (config) {
+//   const token = localStorage.getItem("access_token");
+//   // localStorage.getItem("set_old_access_token")
+//   config.headers = {
+//     "Content-Type": "application/json",
+//   };
+//   if (token) config.headers.Authorization = `${token}`;
+//   return config;
+// });
 
-const refreshAccessToken = (refresh_token) =>
-  axiosInstance.post("auth/refresh", { refresh_token });
+// // Add a response interceptor || Middleware for 401
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     const {
+//       config,
+//       response: { status },
+//     } = error;
+//     const originalRequest = config;
+//     if (status === 401) {
+//       const refresh_token = localStorage.getItem("refresh_token");
+//       if (refresh_token) {
+//         // Refresh Token Promise
+//         if (!isAlreadyFetchingAccessToken) {
+//           isAlreadyFetchingAccessToken = true;
+
+//           refreshAccessToken(refresh_token)
+//             .then((response) => {
+//               isAlreadyFetchingAccessToken = false;
+//               // Replacing Tokens
+//               localStorage.removeItem("access_token");
+//               localStorage.removeItem("refresh_token");
+//               localStorage.setItem(
+//                 "access_token",
+//                 response.data.data.access_token
+//               );
+//               localStorage.setItem(
+//                 "refresh_token",
+//                 response.data.data.refresh_token
+//               );
+//               onAccessTokenFetched(response.data.data.access_token);
+//             })
+//             .catch(() => {
+//               isAlreadyFetchingAccessToken = false;
+//               // Removing User State
+//               localStorage.removeItem("access_token");
+//               localStorage.removeItem("refresh_token");
+
+//               // showToast("Error !", error.response.data.message, "error");
+//               // toast.error(ErrorToast(error.response.data.message), {
+//               //   hideProgressBar: true,
+//               //   autoClose: "100",
+//               // });
+//               setTimeout(() => {
+//                 window.location.reload();
+//               }, 500);
+//             });
+//         }
+
+//         const retryOriginalRequest = new Promise((resolve) => {
+//           addSubscriber((accessToken) => {
+//             // Make sure to assign accessToken according to your response.
+//             // Change Authorization header
+//             originalRequest.headers["Authorization"] = `${accessToken}`;
+//             resolve(axios(originalRequest));
+//           });
+//         });
+//         return retryOriginalRequest;
+//       } else {
+//         return Promise.reject(error);
+//       }
+//     } else {
+//       return Promise.reject(error);
+//     }
+//   }
+// );
+
+// const onAccessTokenFetched = (accessToken) => {
+//   subscribers = subscribers.filter((callback) => callback(accessToken));
+// };
+
+// const addSubscriber = (callback) => {
+//   subscribers.push(callback);
+// };
+
+// const refreshAccessToken = (refresh_token) =>
+//   axiosInstance.post("auth/refresh", { refresh_token });
--- services/service.js
+++ services/service.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { axiosInstance } from "./axiosConfig";
+import axiosInstance from "./axiosConfig";
 
 export const login = async (data) => {
   const response = await axiosInstance.post(
