5fa4cbdfd0e345b53400f00f8e4c2014176f3b9b
Divyengamit <divyengamit@inventam.com>
2022-06-20 14:06:16 +0530

    remove log

--- components/components/RouteGuard.js
+++ components/components/RouteGuard.js
@@ -42,8 +42,8 @@ function RouteGuard({ children }) {
     ];
     const path = url.split("?")[0];
 
-    console.log("path", path);
-    console.log("log", getLocal("access_token"));
+    // console.log("path", path);
+    // console.log("log", getLocal("access_token"));
 
     if (path.includes("redirect") && !publicPaths.includes(path)) {
       setAuthorized(true);
